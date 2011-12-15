78fb86a63b52d1a7e3334c280b435af3768d1b39
Christopher Schwardt <nookieman@gmx.de>
2011-12-15 16:33:43 +0100

    added support for existing bibtex files

--- bibtexMaker.py
+++ bibtexMaker.py
@@ -3,7 +3,8 @@
 import re
 from sets import Set
 
-CITEREGEX = re.compile("\\cite{([^}]+)}", re.M | re.I | re.S)
+LATEXCITEREGEX = re.compile("\\cite{([^}]+)}", re.M | re.I | re.S)
+BIBCITEREGEX = re.compile("@[^{]+{([^,]+),", re.M | re.I | re.S)
 CITETEMPLATE = """
 @BOOK{%s,
 AUTHOR="",
@@ -12,12 +13,18 @@ YEAR=,
 }
 """
 
-def getCitesForFile(f):
+def getCitesForLatexFile(f):
     cites = Set()
-    for cite in CITEREGEX.findall(open(f).read()):
-        if not cite in cites:
-            cites.add(cite)
+    for cite in LATEXCITEREGEX.findall(open(f).read()):
+        cites.add(cite)
     return cites
+
+def getCitesForBibFile(f):
+    cites = Set()
+    for cite in BIBCITEREGEX.findall(open(f).read()):
+        cites.add(cite.strip())
+    return cites
+
 
 def createBibtexFile(outfile, cites):
     fh = open(outfile, "w+")
@@ -35,9 +42,14 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description="create a prefilled bibtex file from latex documents")
     parser.add_argument('files', metavar="FILE", type=str, nargs="+", help="LaTeX files to parse for citations")
     parser.add_argument('-o', '--outfile', dest="outfile", type=str, default="bibtex.bib", help="output file for the bibtex (default: bibtex.bib)")
+    parser.add_argument('-b', '--bib', dest="bibs", type=str, default=[], action="append", help="bib files to check for already defined references")
     args = parser.parse_args()
 
-    cites = Set()
+    neededCites = Set()
     for f in args.files:
-        cites.update(getCitesForFile(f))
+        neededCites.update(getCitesForLatexFile(f))
+    definedCites = Set()
+    for f in args.bibs:
+        definedCites.update(getCitesForBibFile(f))
+    cites = neededCites - definedCites
     createBibtexFile(args.outfile, cites)
