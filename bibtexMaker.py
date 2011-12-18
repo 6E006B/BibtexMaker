00d608a3505c36df0427d9cc3dd932da6ea0e8b5
Christopher Schwardt <nookieman@gmx.de>
2011-12-18 23:03:23 +0100

    added flag to replace outfile

--- bibtexMaker.py
+++ bibtexMaker.py
@@ -26,8 +26,9 @@ def getCitesForBibFile(f):
     return cites
 
 
-def createBibtexFile(outfile, cites, citeType):
-    fh = open(outfile, "a")
+def createBibtexFile(outfile, cites, citeType, replace):
+    mode = "w" if replace else "a"
+    fh = open(outfile, mode)
     for cite in cites:
         writeCitation(fh, cite, citeType)
     fh.close()
@@ -44,6 +45,7 @@ if __name__=="__main__":
     parser.add_argument('-o', '--outfile', dest="outfile", type=str, default="bibtex.bib", help="output file for the bibtex (default: bibtex.bib)")
     parser.add_argument('-b', '--bib', dest="bibs", type=str, default=[], action="append", help="bib files to check for already defined references")
     parser.add_argument('-t', '--type', dest="citeType", type=str, default="BOOK", help="define the type of citation for the Bibtex file (default: BOOK)")
+    parser.add_argument('-r', '--replace', dest="replace", action="store_true", default=False, help="replace the outfile if it exists (default: append to outfile)")
     args = parser.parse_args()
 
     neededCites = Set()
@@ -53,4 +55,4 @@ if __name__=="__main__":
     for f in args.bibs:
         definedCites.update(getCitesForBibFile(f))
     cites = neededCites - definedCites
-    createBibtexFile(args.outfile, cites, args.citeType)
+    createBibtexFile(args.outfile, cites, args.citeType, args.replace)
